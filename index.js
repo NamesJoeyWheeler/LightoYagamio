46348ff2ba4ca24f270dd8c9743938445640bd5f
NamesJoeyWheeler <36152493+NamesJoeyWheeler@users.noreply.github.com>
2018-06-05 18:00:45 +1000

    Update index.js

--- index.js
+++ index.js
@@ -50,9 +50,10 @@ client.on('message', msg => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'Will you be god?') {
-    msg.channel.send('/tts WILL BE?! I AM GOD OF THE NEW WORLD!');
+  if (msg.content === 'Who has a death note?') {
+    msg.author.send('I have one. Who do you want to be killed?');
   }
 });
+
 
 client.login(process.env.BOT_TOKEN);
