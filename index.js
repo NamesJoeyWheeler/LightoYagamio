cc692a4cee50497214d57622fd9a77b2320035bf
NamesJoeyWheeler <36152493+NamesJoeyWheeler@users.noreply.github.com>
2018-06-05 17:41:36 +1000

    Update index.js

--- index.js
+++ index.js
@@ -51,8 +51,8 @@ client.on('message', msg => {
 
 
 client.on('message', msg => {
-  if (msg.content === 'Light, Catch a pokemon') {
-    msg.reply('p!start');
+  if (msg.content === 'Talk Light') {
+    msg.reply('/tts I WILL BE GOD OF THE NEW WORLD');
   }
 });
 client.login(process.env.BOT_TOKEN);
