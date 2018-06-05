908bde0dbbcb08f4ef1e00cf56efe49c703b4ccd
NamesJoeyWheeler <36152493+NamesJoeyWheeler@users.noreply.github.com>
2018-06-05 17:58:05 +1000

    Update index.js

--- index.js
+++ index.js
@@ -49,4 +49,10 @@ client.on('message', msg => {
   }
 });
 
+client.on('message', msg => {
+  if (msg.content === 'Will you be god?') {
+    msg.channel.send('/tts WILL BE?! I AM GOD OF THE NEW WORLD!');
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
